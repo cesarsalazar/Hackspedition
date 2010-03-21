8de690e5e0915aff4d8ef3a25d4f9e0712985e19
César Salazar <csr.slzr@gmail.com>
2010-03-21 00:34:01 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<title>Hackspediton - Let's hack together!</title>
+	<link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+	<body>
+		<div id="header-container">
+			<div id="header" class="center">
+				<h1 id="logo">Hackspedition</h1>
+				<p class="big">Hackspeditions are open traveling events oriented to cross pollination between two or more hacker communities around the world.</p>
+			</div>
+		</div>
+		<div id="main-container" class="center">
+			<div id="main">
+				<div id="sv" class="highlight">
+					<p class="big align-center">Hackspedition Silicon Valley is <a href="sv">Coming Soon</a>.</p>
+				</div>
+				<h2>Start your own</h2>
+					<p>Hackspedition is an open framework. Yo can use it, adapt it and contribute to it.</p>
+					<p>You can follow our guidelines. Here we hand you some resources and some history that might be of some use.</p>
+					<p>If you have further questions, please contact us at <a href="mailto:lets.do.it@hackspedition.org">lets.do.it@hackspedition.org</a></p>
+				<h2>Basic principles</h2>
+					<ul>
+						<li>Hackspedition should be open and non-exclusive.</li>
+						<li>There should be space and time to do some actual social hacking.</li>
+						<li>There should be leisure/adventure activities that have nothing to do with computers.</li>
+						<li>The event should involve at least two communities from two different geographies</li>
+					</ul>
+				<h2>Guidelines</h2>
+					<ol>
+						<li>Organize a three to five day trip. One of the communities can host the other community</li>
+						<li>Don't bother to  book hotel rooms. Ideally, guest hackers can borrow a couch or mattress from host hackers.</li>
+						<li>A welcome party is awesome. Reserve a small bar and go for a couple of drinks together the first night.</li>
+						<li>Sightseeing is good, exploration is better. Most hackers are very curious people regardless the situation. Give them enough materials to make the experience as insightful as possible.</li>
+						<li>Mix, mix, mix. The whole point of Hackspeditions is to cross-pollinate. Do whatever is necessary to beat language or interpersonal barriers.</li>
+						<li>Mind about cultural differences. Take some time to discuss the agenda with people from both communities.</li>
+						<li>Hack together. Try to have a SuperHappyDevHouse or similar event built into the event.</li>
+						<li>Push your limits. People really get fond of each other when they live intense experiences together. Adventure activities such as rafting, hiking or canoeing are perfect to achieve this.</li>
+						<li>Run a wrap up session. Make sure everybody is aware of the achievements made during the Hackspedition. Also make sure people have the means to stay in touch.</li>
+						<li>Do. Learn. Share. Iterate.</li>
+					</ol>
+				<h2>Our first time</h2>
+					<p>If you want to learn more about the first Hackspedition, <a href="http://hackspedition.pbworks.com" target="blank">please visit the wiki</a>.</p>
+			</div>
+		</div>
+		<div id="footer-container">
+			<div id="footer" class="center">
+				<p>The Hackspedition Brand is owned and powered by the Hackspedition Community. Please don't steal it.</p>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ sv.php
@@ -0,0 +1,86 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head lang="ES-mx">
+	<title>Hackspediton - Let's hack together!</title>
+	<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+	<link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+	<body class="single">
+		<div id="header-container">
+			<div id="header" class="center">
+				<h1 id="logo">Hackspedition Silicon Valley</h1>
+				<p class="big">¿Qué tal conocer Twitter, admirar el Golden Gate, asistir a un SHDH y surfear en un fin de semana?</p>
+				<div id="pics">
+					<img src="images/surf.jpg"/>
+					<img src="images/shdh.jpg"/>
+					<img src="images/hxpn.jpg"/>
+				</div>
+			</div>
+		</div>
+		<div id="main-container" class="center">
+			<div id="main">
+				<p>Leíste bien. Se viene la Hackspedition Silicon Valley. Como tal vez ya sabes, la Hackpedition es una aventura intercultural cuya finalidad es reunir comunidades de hackers de distintas partes del mundo.</p>
+				<p>En la Hackspedition podrás conocer toda clase de desarrolladores y emprendedores que se ganan la vida creando cosas nuevas en el epicentro tecnológico del mundo.</p>
+				<p>Si quieres participar, simplemente apúntate en el <a href="https://hackspedition.pbworks.com/SiliconValley" target="blank">wiki</a> y empieza a ahorrar. Por motivos logísticos sólo podremos aceptar inscripciones hasta el 30 de mayo, por lo cual te sugerimos que te no pierdas tiempo.</p>
+				<p class="big align-center">¿No sabes qué es Hackspedition? <a href="index">Conoce más</a>.</p>
+				<hr/>
+				<div id="logos">
+					<p class="big align-center">Visitas y sesiones en:</p>
+					<img src="images/microsoft.jpg"/>
+					<img src="images/twitter.jpg"/>
+					<img src="images/google.jpg"/>
+					<img src="images/twilio.jpg"/>
+					<img src="images/pbworks.jpg"/>
+					<img src="images/techbasv.jpg"/>
+					<img src="images/hackerdojo.jpg"/>
+					<img src="images/shdh_logo.jpg"/>
+					<img src="images/imvu.jpg"/>
+					<img src="images/280north.jpg"/>
+				</div>
+				<hr/>
+				<h2>Programa</h2>
+				<p class="note">(Ten en cuenta que el programa aún es tentativo)</p>
+				<h3>Jueves</h3>
+				<p>Salida de la Ciudad de México (Toluca)</p>
+				<p>Llegada al Área de la Bahía de San Francisco (Silicon Valley) - Cena de bienvenida.</p>
+				<h3>Viernes</h3> 
+				<p>Visita a Microsoft Research Center</p>
+				<p>Visita a Hacker Dojo</p>
+				<p>Visita a Google</p>
+				<p>Visita a IMVU</p>
+				<p>Visita a TechBA</p>
+				<h3>Sábado</h3>
+				<p>Visita de Ciudad en San Francisco</p>
+				<p>SuperHappyDevHouse 38</p>
+				<h3>Domingo</h3>
+				<p>Salida a Santa Cruz, CA - Surfing (Yeah!)</p>
+				<h3>Lunes</h3>
+				<p>Visita a Twitter</p> 
+				<p>Visita a 280 North</p> 
+				<p>Visita a Twilio</p>
+				<p>Visita a PBworks</p>
+				<h3>Martes</h3>
+				<p>Desayuno de despedida</p>
+				<p>Regreso a la Ciudad de México (Toluca)</p>
+				<h2>Costo</h2>
+				<p>El costo de la Hackspedition se compone básicamente de avión, presupuesto para comidad diarias, renta de equipo para actividades en Santa cruz. Estamos trabajando con Volaris, para obtener una tarifa especial, pero te pedimos que presupuestes en base a la tarifa normal.</p>
+				<p>El hospedaje y la transportación en el lugar serán amablemente provistos por nuestros amigos anfitriones.</p>
+				<ul>
+					<li>Vuelo redondo: $300 USD</li>
+					<li>Comidas diarias: $20 x 5 USD</li>
+					<li>Renta de equipo: $50 USD</li>
+				</ul>
+				<p>Total: $550 USD</p>
+				<h2>Fecha</h2>
+				<p>Primavera 2010. Estamos planeando que sea en junio, pero la fecha queda por confirmarse.</p>
+				<h2>Información adicional</h2>
+				<p>Para solicitar información adicional, por favor escríbenos a <a>sv@hackspedition.org</a></p>
+			</div>
+		</div>
+		<div id="footer-container">
+			<div id="footer" class="center">
+				<p>The Hackspedition Brand is owned and powered by the Hackspedition Community. Please don't steal it.</p>
+			</div>
+		</div>
+	</body>
+</html>
