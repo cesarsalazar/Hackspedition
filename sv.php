a835c27eba398b71034f1531f0db59f20d3f89b7
César Salazar <csr.slzr@gmail.com>
2010-06-17 07:57:14 -0500

    Changes to SV

--- sv.php
+++ sv.php
@@ -9,6 +9,10 @@
 		<div id="header-container">
 			<div id="header" class="center">
 				<h1 id="logo">Hackspedition Silicon Valley</h1>
+				<div id="dates" class="highlight">
+					<p class="big align-center">La Hackspedition será del <s>24</s> 23 al 28 de junio. <a href="http://spreadsheets.google.com/viewform?formkey=dFQySkRPMC03SnpEWW82ZWVSRFJUWUE6MQ" target="blank">Reserva tu lugar ahora</a>.</p>
+					<p class="align-center"><em>No dejes de leer los detalles (publicados más abajo).</em></p>
+				</div>
 				<p class="big">¿Qué tal conocer Twitter, admirar el Golden Gate, asistir a un SHDH y surfear en un fin de semana?</p>
 				<div id="pics">
 					<img src="images/surf.jpg"/>
@@ -21,60 +25,67 @@
 			<div id="main">
 				<p>Leíste bien. Se viene la Hackspedition Silicon Valley. Como tal vez ya sabes, la Hackpedition es una aventura intercultural cuya finalidad es reunir comunidades de hackers de distintas partes del mundo.</p>
 				<p>En la Hackspedition podrás conocer toda clase de desarrolladores y emprendedores que se ganan la vida creando cosas nuevas en el epicentro tecnológico del mundo.</p>
-				<p>Si quieres participar, simplemente apúntate en el <a href="https://hackspedition.pbworks.com/SiliconValley" target="blank">wiki</a> y empieza a ahorrar. Por motivos logísticos sólo podremos aceptar inscripciones hasta el 30 de mayo, por lo cual te sugerimos que te no pierdas tiempo.</p>
 				<p class="big align-center">¿No sabes qué es Hackspedition? <a href="index">Conoce más</a>.</p>
 				<hr/>
 				<div id="logos">
 					<p class="big align-center">Visitas y sesiones en:</p>
 					<img src="images/microsoft.jpg"/>
-					<img src="images/twitter.jpg"/>
-					<img src="images/google.jpg"/>
-					<img src="images/twilio.jpg"/>
-					<img src="images/pbworks.jpg"/>
 					<img src="images/techbasv.jpg"/>
-					<img src="images/hackerdojo.jpg"/>
 					<img src="images/shdh_logo.jpg"/>
+					<img src="images/hackerdojo.jpg"/>
+					<img src="images/pbworks.jpg"/>
 					<img src="images/imvu.jpg"/>
 					<img src="images/280north.jpg"/>
+					<img src="images/google.jpg"/>
+					<img src="images/twilio.jpg"/>
+					<img src="images/twitter.jpg"/>								
 				</div>
 				<hr/>
+				<h2>Fechas</h2>
+				<p>Del <s>24</s> 23 de junio al 28 de junio de 2010</p>
 				<h2>Programa</h2>
 				<p class="note">(Ten en cuenta que el programa aún es tentativo)</p>
-				<h3>Jueves</h3>
-				<p>Salida de la Ciudad de México (Toluca)</p>
-				<p>Llegada al Área de la Bahía de San Francisco (Silicon Valley) - Cena de bienvenida.</p>
-				<h3>Viernes</h3> 
+				<h3>Miércoles 23 de junio</h3>
+				<p>Salida de la Ciudad de México (MEX) - 14:00</p>
+				<p>Llegada al Área de la Bahía de San Francisco (SFO) - 18:50</p>
+				<p>Cena de bienvenida.</p>
+				<h3>Jueves 24 de junio</h3>
+				<p>Visita a TechBA</p>
 				<p>Visita a Microsoft Research Center</p>
-				<p>Visita a Hacker Dojo</p>
 				<p>Visita a Google</p>
 				<p>Visita a IMVU</p>
-				<p>Visita a TechBA</p>
-				<h3>Sábado</h3>
-				<p>Visita de Ciudad en San Francisco</p>
-				<p>SuperHappyDevHouse 38</p>
-				<h3>Domingo</h3>
-				<p>Salida a Santa Cruz, CA - Surfing (Yeah!)</p>
-				<h3>Lunes</h3>
-				<p>Visita a Twitter</p> 
-				<p>Visita a 280 North</p> 
-				<p>Visita a Twilio</p>
+				<h3>Viernes 25 de junio</h3> 
 				<p>Visita a PBworks</p>
-				<h3>Martes</h3>
-				<p>Desayuno de despedida</p>
-				<p>Regreso a la Ciudad de México (Toluca)</p>
+				<p>Visita a Twitter</p> 		
+				<p>Visita a 280 North</p>
+				<p>Visita a Twilio</p>
+				<p>Visita de Ciudad en San Francisco</p>
+				<h3>Sábado 26 de junio</h3>
+				<p>Visita a Ames Research Center/NASA</p>
+				<p>SuperHappyDevHouse 38</p>
+				<h3>Domingo 27 de junio</h3>
+				<p>Salida a Santa Cruz, CA - Surfing (Yeah!)</p>
+				<h3>Lunes 28 de junio</h3>
+				<p>Regreso a la Ciudad de México (SFO) - 00:40 am</p>
+				<p>Llegada a la Ciudad de México (MEX) - 8:50 am</p>
 				<h2>Costo</h2>
-				<p>El costo de la Hackspedition se compone básicamente de avión, presupuesto para comidad diarias, renta de equipo para actividades en Santa cruz. Estamos trabajando con Volaris, para obtener una tarifa especial, pero te pedimos que presupuestes en base a la tarifa normal.</p>
+				<p>El costo de la Hackspedition se compone básicamente de avión (conseguimos un precio de grupo con Volaris), presupuesto para comidad diarias y renta de equipo para actividades en Santa cruz.</p>
 				<p>El hospedaje y la transportación en el lugar serán amablemente provistos por nuestros amigos anfitriones.</p>
 				<ul>
-					<li>Vuelo redondo: $300 USD</li>
+					<li>Vuelo redondo: <s>$5,420.00</s> <strong>$4,820.00</strong>(Ver "Notas")</li>
 					<li>Comidas diarias: $20 x 5 USD</li>
 					<li>Renta de equipo: $50 USD</li>
 				</ul>
-				<p>Total: $550 USD</p>
-				<h2>Fecha</h2>
-				<p>Primavera 2010. Estamos planeando que sea en junio, pero la fecha queda por confirmarse.</p>
+				<p>Total: $590 USD (Aproximadamente)</p>
+				<h2>Notas</h2>
+				<ul>
+					<li>El vuelo redondo fue cotizado con <s>Volaris en base a un descuento de grupo</s> Mexicana. <s>Para poder acceder a esta tarifa, es necesario hacer el pago total del vuelo <strong>a más tardar el 19 de mayo</strong></s>.</li>
+					<li>Si deseas volar con otra aerolínea y/o en otro horario, puedes hacerlo, pero tú serás responsable de unirte al grupo donde quiera que este se encuentre al momento de tu llegada.</li>
+					<li><s>El pago del vuelo lo puedes hacer a la cuenta <strong>1402291-5</strong> de <strong>IXE Banco</strong> a nombre de <strong>César Eduardo Salazar Hernández</strong> o con la CLABE <strong>032180000140229150</strong></s> Si deseas volar en este itinerario, deberás reservar tu lugar directamente con la aerolínea.</li>
+					<li>Para poder asistir a la Hackspetion deberás <a href="http://spreadsheets.google.com/viewform?formkey=dFQySkRPMC03SnpEWW82ZWVSRFJUWUE6MQ" target="blank">reservar tu lugar aquí</a>.</li>
+				</ul>
 				<h2>Información adicional</h2>
-				<p>Para solicitar información adicional, por favor escríbenos a <a>sv@hackspedition.org</a></p>
+				<p>Para solicitar información adicional, por favor escríbenos a <a href="mailto:sv@hackspedition.org">sv@hackspedition.org</a>.</p>
 			</div>
 		</div>
 		<div id="footer-container">
